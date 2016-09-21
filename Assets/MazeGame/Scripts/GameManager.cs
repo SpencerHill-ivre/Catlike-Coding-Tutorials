f6c8204fc3d4b88609a037c93e3d21deb796c15f
Spencer Hill <spencer.george.hill@gmail.com>
2016-09-21 19:41:20 +0100

    Initial Commit: -Clock -fpsDisplay -Nucleon Spawner (to test fpsDisplay) -MazeGame (Maze generator (single file corridors only))

---
+++ Assets/MazeGame/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+	void Start ()
+    {
+        BeginGame();
+	}
+	
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+	}
+
+    void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
